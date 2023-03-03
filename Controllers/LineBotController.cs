d6856db51e17b15005c9b6b754492c94b9a4df35
1502TOMMYPC-1\Tommy <xxs4129@gmail.com>
2023-03-03 15:23:37 +0800

    更新

--- Controllers/LineBotController.cs
+++ Controllers/LineBotController.cs
@@ -3,6 +3,7 @@ using isRock.LineBot;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ChatGptWebAPI.Controllers
 {
@@ -11,12 +12,10 @@ namespace ChatGptWebAPI.Controllers
     public class LineBotController : LineWebHookControllerBase
     {
         public const string adminUserId = "Ue27cd4c8df1ef9e6766b5cb6875a0d0f";
-        public ChatGPTClient client;
 
         public LineBotController()
         {
             this.ChannelAccessToken = "TNwTq+/3kNrVZN+v7A+T0+9ci3trSdCnsHhdyNTwsElhiJvW1JCH+5o8UWR2aFBsmMola+E1DqX2+35uNRqjVFZDTrj3LrihbdIST2d69QVhe7I70sQOZuTlOgEtkKRix1XD6/yCIkq2SQWsNuvO6gdB04t89/1O/w1cDnyilFU=";
-            client = new ChatGPTClient("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", "gpt-3.5-turbo");
         }
 
         /// <summary> 
@@ -32,23 +31,44 @@ namespace ChatGptWebAPI.Controllers
         [Route("api/ChatGPT")]
         [HttpPost]
         [Produces("application/json")]
-        //[HttpPost(Name = "ChatGPT")]
         public async Task<IActionResult> ChatGPT()
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             try
             {
                 var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
+                ChatGPTClient client = new ChatGPTClient("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", "gpt-3.5-turbo");
 
                 var msg = await client.SendMessage(LineEvent.message.text);
 
                 this.ReplyMessage(LineEvent.replyToken, msg.Response);
             }
-            catch(Exception ex) 
+            catch (Exception ex)
             {
                 this.PushMessage(adminUserId, "發生錯誤:\n" + ex.Message);
             }
             return Ok();
         }
+
+        //[Route("api/test")]
+        //[HttpGet]
+        //[Produces("application/json")]
+        //public async Task<IActionResult> test()
+        //{
+        //    try
+        //    {
+        //        var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
+        //        ChatGPTClient client = new ChatGPTClient("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", "gpt-3.5-turbo");
+
+        //        var msg = await client.SendMessage(LineEvent.message.text);
+
+        //        this.ReplyMessage(LineEvent.replyToken, msg.Response);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        this.PushMessage(adminUserId, "發生錯誤:\n" + ex.Message);
+        //    }
+        //    return Ok();
+        //}
     }
 }
