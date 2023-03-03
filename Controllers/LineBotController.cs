aede24cd5ded9f5d52ef0a4470440d75f62ea8be
1502TOMMYPC-1\Tommy <xxs4129@gmail.com>
2023-03-03 17:14:44 +0800

    新增/img 指令產生圖片

--- Controllers/LineBotController.cs
+++ Controllers/LineBotController.cs
@@ -1,17 +1,21 @@
-﻿using ChatGPTSharp;
-using isRock.LineBot;
+﻿using isRock.LineBot;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace ChatGptWebAPI.Controllers
 {
-    //[ApiController]
-    //[Route("[controller]")]
     public class LineBotController : LineWebHookControllerBase
     {
         public const string adminUserId = "Ue27cd4c8df1ef9e6766b5cb6875a0d0f";
+        public static List<Message> chats = new List<Message>();
 
         public LineBotController()
         {
@@ -37,11 +41,57 @@ namespace ChatGptWebAPI.Controllers
             try
             {
                 var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
-                ChatGPTClient client = new ChatGPTClient("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", "gpt-3.5-turbo");
 
-                var msg = await client.SendMessage(LineEvent.message.text);
+                Console.WriteLine(LineEvent.message.text);
 
-                this.ReplyMessage(LineEvent.replyToken, msg.Response);
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "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");
+                    httpClient.DefaultRequestHeaders.Add("OpenAI-Organization", "org-NcvEcjsvwKLpvuooIKIJBQAx");
+
+                    if (LineEvent.message.text.StartsWith("/img "))
+                    {
+                        JObject requestData = new JObject();
+                        requestData.Add("model", "image-alpha-001");
+                        requestData.Add("prompt", LineEvent.message.text.TrimStart("/img ".ToArray()));
+                        requestData.Add("num_images", 1);
+                        var content = new StringContent(requestData.ToString(), System.Text.Encoding.UTF8, "application/json");
+
+                        var response = await httpClient.PostAsync("https://api.openai.com/v1/images/generations", content);
+                        response.EnsureSuccessStatusCode();
+
+                        var responseContent = await response.Content.ReadAsStringAsync();
+
+                        JObject jsonResponse = JObject.Parse(responseContent);
+
+                        Utility.ReplyImageMessage(LineEvent.replyToken, jsonResponse["data"][0]["url"].ToString(), jsonResponse["data"][0]["url"].ToString(), this.ChannelAccessToken);
+                    }
+                    else 
+                    {
+                        chats.Add(new Message() { role = "user", content = LineEvent.message.text });
+                        parameters requestBody = new parameters
+                        {
+                            model = "gpt-3.5-turbo",
+                            messages = chats
+                        };
+                        var json = JsonConvert.SerializeObject(requestBody);
+                        var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+                        var response = await httpClient.PostAsync("https://api.openai.com/v1/chat/completions", content);
+                        response.EnsureSuccessStatusCode();
+
+                        var responseJson = await response.Content.ReadAsStringAsync();
+                        Root responseData = JsonConvert.DeserializeObject<Root>(responseJson);
+                        chats.Add(responseData.choices[0].message);
+
+                        Console.WriteLine(responseData.choices[0].message.content);
+                        Utility.ReplyMessage(LineEvent.replyToken, responseData.choices[0].message.content, this.ChannelAccessToken);
+
+                        Console.WriteLine(chats.Count);
+                    }
+                    
+                }
+
             }
             catch (Exception ex)
             {
@@ -50,25 +100,50 @@ namespace ChatGptWebAPI.Controllers
             return Ok();
         }
 
+
         //[Route("api/test")]
         //[HttpGet]
         //[Produces("application/json")]
         //public async Task<IActionResult> test()
         //{
-        //    try
-        //    {
-        //        var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
-        //        ChatGPTClient client = new ChatGPTClient("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", "gpt-3.5-turbo");
+           
 
-        //        var msg = await client.SendMessage(LineEvent.message.text);
-
-        //        this.ReplyMessage(LineEvent.replyToken, msg.Response);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        this.PushMessage(adminUserId, "發生錯誤:\n" + ex.Message);
-        //    }
         //    return Ok();
         //}
+
+        class parameters
+        {
+            public string model { get; set; }
+            public List<Message> messages { get; set; }
+        }
+        public class Choice
+        {
+            public Message message { get; set; }
+            public string finish_reason { get; set; }
+            public int index { get; set; }
+        }
+
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+        }
+
+        public class Root
+        {
+            public string id { get; set; }
+            public string @object { get; set; }
+            public int created { get; set; }
+            public string model { get; set; }
+            public Usage usage { get; set; }
+            public List<Choice> choices { get; set; }
+        }
+
+        public class Usage
+        {
+            public int prompt_tokens { get; set; }
+            public int completion_tokens { get; set; }
+            public int total_tokens { get; set; }
+        }
     }
 }
